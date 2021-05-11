2df1bfe95ae3dab6a9e51896a2fddb1f6950b1ae
Daft Penguin <daftpenguinrl@gmail.com>
2021-05-11 03:09:15 -0400

    v1.1.5 release

--- StreamAPIPlugin/version.h
+++ StreamAPIPlugin/version.h
@@ -1,8 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
-#define VERSION_PATCH 4
-#define VERSION_BUILD 622
+#define VERSION_PATCH 5
+#define VERSION_BUILD 623
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
