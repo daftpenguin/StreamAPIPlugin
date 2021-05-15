ab39385804ac5b4d9e35e3156a28c66ae3988ed1
Daft Penguin <daftpenguinrl@gmail.com>
2021-05-14 20:06:00 -0400

    Fixed workshop command for epic games version

--- StreamAPIPlugin/version.h
+++ StreamAPIPlugin/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
 #define VERSION_PATCH 6
-#define VERSION_BUILD 628
+#define VERSION_BUILD 634
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
